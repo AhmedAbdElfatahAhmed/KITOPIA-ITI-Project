6f9243b80ac246e3ae3f7a1df1162f827bb04c22
Ahmed Abd Elfatah <chem.ahmedaf@gmail.com>
2022-11-03 17:46:48 +0200

    make component for each section in Home

---
+++ src/components/Home/Footer/index.jsx
@@ -0,0 +1,12 @@
+import "./Footer.scss";
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center fs-5">
+        Copyright &copy; 2022 <span>KITOPIA</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
