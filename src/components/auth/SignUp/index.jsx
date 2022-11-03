9766624e1cfd51df982361efad353efb6cfec2b8
Mohamed-Ayish <mohamed2280abdo@gmail.com>
2022-11-03 15:34:45 +0200

    removing css files to use sass

--- src/components/auth/SignUp/index.jsx
+++ src/components/auth/SignUp/index.jsx
@@ -1,4 +1,3 @@
-
 import { useForm } from "react-hook-form";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -12,7 +11,6 @@ import { useState } from "react";
 
 function SignUp() {
   const { register, handleSubmit, formState: { errors } } = useForm();
-
   const [selectedImages, setSelectedImages] = useState([]);
 
   const onSelectFile = (event) => {
@@ -22,29 +20,28 @@ function SignUp() {
     const imagesArray = selectedFilesArray.map((file) => {
       return URL.createObjectURL(file);
     });
+
     setSelectedImages((previousImages) => previousImages.concat(imagesArray));
   };
 
-
-
   const onSubmit = data => console.log(data);
   return (
-    <div className="container">
+    <div className="container cont">
       <div className="row">
-        <div className="col-md-6 mt-5">
-          <div className="image">
-            <img className="  w-100" src={signupImg} alt="" />
+        <div className="col-md-6">
+          <div className="image ">
+            <img className="w-100" src={signupImg} alt="" />
           </div>
         </div>
         <div className="col-md-6">
-          <Form onSubmit={handleSubmit(onSubmit)} className=" p-5 w-100 text-info  mt-5">
+          <Form onSubmit={handleSubmit(onSubmit)} className=" p-5 w-100 ">
 
             <div className="row ">
               {/* parent & child */}
-              <div className="col-md-6  parent-child w-100 m-auto  card py-4">
+              <div className="col-md-6  parent-child w-100 m-auto  card shadow py-4">
                 {/* parent */}
                 <div className="parent d-flex justify-content-around    py-2">
-                  <div className="image mb-3">
+                  <div className="image mb-3 d-flex  align-items-center">
                     <img className="w-100  rounded-circle paimg" src={parentImg} alt="" />
                   </div>
                   <div className="iputs w-50  px-2">
@@ -140,7 +137,7 @@ function SignUp() {
                 </div>
                 {/* child */}
                 <div className="child d-flex justify-content-around     py-2">
-                  <div className="image mb-3">
+                  <div className="image mb-3 d-flex  align-items-center">
                     <img className="w-100 rounded-circle chimg" src={childImg} alt="" />
                   </div>
                   <div className="iputs w-50 px-2">
@@ -190,29 +187,38 @@ function SignUp() {
                     {/* upload image*/}
                     <Form.Group className="mb-3" controlId="formBasicimage">
                       <section>
-                        <div className="uploading card text-center p-2">
-                          <p className="text-dark">Upload Image </p>
-                          <div className="d-flex ">
-                            <div className="input">
+                        <div className="uploading  card p-1 text-center text-dark">
+                          <p>Uploade Image</p>
+                          <div className="row ">
+                            <div className="col-md-6 ">
                               <input
-                                className="text-dark w-75"
                                 type="file"
+                                className="w-100 text-light"
                                 onChange={onSelectFile}
                               />
                             </div>
-                            <div className="images w-100">
-                              {selectedImages &&
-                                selectedImages.map((image) => {
-                                  return (
-                                    <div key={image} className="image">
-                                      <img src={image} className="w-100 m-auto card" alt="upload" />
-                                    </div>
-                                  );
-                                })}
+                            <div className="col-md-6 ">
+                              <div className="images w-75">
+                                {selectedImages &&
+                                  selectedImages.map((image) => {
+                                    return (
+                                      <div key={image} className="image ">
+                                        <img src={image} className="w-100 card " alt="upload" />
+
+                                      </div>
+                                    );
+                                  })}
+                              </div>
                             </div>
                           </div>
                         </div>
                       </section>
+                      {console.log(errors)}
+                      {
+                        errors?.email?.type === "required"
+                        &&
+                        <p className="text-danger">phone number is required</p>
+                      }
 
                     </Form.Group>
                   </div>
@@ -221,10 +227,10 @@ function SignUp() {
               </div>
             </div>
             <div className="submit text-center ">
-              <Button className="mt-5 sign px-5" type="submit">
+              <Button className="mt-3 sign px-5" type="submit">
                 Sign Up
               </Button>
-              <p className="mt-4">Already have an account? <span> <Link className='text-decoration-none   '
+              <p className="">Already have an account? <span> <Link className='text-decoration-none'
                 to="/login">Login</Link></span></p>
             </div>
           </Form>
