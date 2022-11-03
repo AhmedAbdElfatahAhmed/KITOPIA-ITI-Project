228b57f932f04a52dce604f4bfa4ce66a3b37e67
Ahmed Abd Elfatah <chem.ahmedaf@gmail.com>
2022-11-03 19:24:28 +0200

    resolve conflict

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,16 @@
-import { Fragment } from "react";
-import ParentFeedback from "./ParentFeedback";
-import ContactUS from "./ContactUS";
-import "./Home.scss";
-const Home = () => {
-  return (
-    <Fragment>
-      <main>
-        <ParentFeedback></ParentFeedback>
-        <ContactUS></ContactUS>
-      </main>
-    </Fragment>
-  );
-};
+// import { Fragment } from "react";
+// import ParentFeedback from "./ParentFeedback";
+// import ContactUS from "./ContactUS";
+// import "./Home.scss";
+// const Home = () => {
+//   return (
+//     <Fragment>
+//       <main>
+//         <ParentFeedback></ParentFeedback>
+//         <ContactUS></ContactUS>
+//       </main>
+//     </Fragment>
+//   );
+// };
 
-export default Home;
+// export default Home;
