0affdc7c279c65b2610d6d423cf9274bfbefbf51
Ahmed Abd Elfatah <chem.ahmedaf@gmail.com>
2022-11-09 05:04:15 +0200

    fix bug in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,9 @@
-import "./Footer.scss";
-import footerstrap from "../../assets/images/footer/footerstrap.png"
+import footerstrap from "../../assets/images/footer/footerstrap.png";
 const Footer = () => {
   return (
-    <footer  className="position-relative">
-      <figure className="w-100 position-absolute">
-      <img className="w-100" src={footerstrap} alt="footerstrap" />
+    <footer className="mt-auto">
+      <figure className="w-100 m-0">
+        <img className="w-100" src={footerstrap} alt="footerstrap" />
       </figure>
     </footer>
   );
