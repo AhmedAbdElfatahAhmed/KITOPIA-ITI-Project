3c7e15e3d7b38502cf9bf1164621cb5e6bd3ff01
mahmoudT95 <mahmoud.t1995@gmail.com>
2022-11-09 21:11:00 +0200

    fixing profile responsice

--- src/middleware/fetch.js
+++ src/middleware/fetch.js
@@ -11,7 +11,7 @@ const baseURL =`https://youtube-v3-alternative.p.rapidapi.com/search`
     url: baseURL ,
     params: { geo: 'US', lang: 'en'  , type: 'video' , sort_by: 'relevance'},
     headers: {
-      'X-RapidAPI-Key': '4437b4cf0bmsh853167890a039dep1ff7fbjsn3fd485dc9031',
+      'X-RapidAPI-Key': '3fdbee7856msh6aea22bd9c428b2p16efdejsneaeee50a53f7',
       'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
     }
   };
